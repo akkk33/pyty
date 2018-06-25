855dd83ac6d811e0f3702b2213124077e97e8400
akkk33 <akkk33@protonmail.com>
2018-06-25 06:36:26 +0200

    PyGTK

--- PyGTK/main.py
+++ PyGTK/main.py
@@ -1,9 +1,21 @@
 import gi
 
-gi.require_version("Gtk", "2.0")
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-win = Gtk.Window()
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title = "Hi")
+        self.button = Gtk.Button(label = "Click me")
+        self.button.connect("clicked", self.on_button_click)
+        self.add(self.button)
+
+    def on_button_click(self, widget):
+        print("Hello world")
+
+
+win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
