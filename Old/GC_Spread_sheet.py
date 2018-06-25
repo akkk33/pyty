e2d8308181f1107542ddffbc2b15e2a3af87ee2c
akkk33 <akkk33@protonmail.com>
2018-06-25 04:51:02 +0200

    OOP workout

--- Old/GC_Spread_sheet.py
+++
@@ -1,6 +0,0 @@
-formula = "=B#*5+C#*#+D#*1#+E#*3+F#*5+G#*4+H#*4+I#*3٫5+J#*4+K#*4+L#*2٫5+M#*5+N#"
-i = 3
-while i <= 32:
-    convert = formula.replace('#', str(i))
-    print(convert,end="\n")
-    i = i + 1
--- db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3, os
-con = sqlite3.connect(os.getcwd() + '/db.sqlite3')
-cursor = con.cursor()
-# cursor.execute("INSERT INTO Users(ID, Name, Age) VALUES(1, 'Amr', 24)")
-cursor.execute("SELECT * FROM Users")
-print(cursor.fetchall())
-con.commit()
-cursor.close()
-con.close()
