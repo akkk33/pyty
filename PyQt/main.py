612357f39e8cbee66cd04cec7e5c09d01bd66007
akkk33 <akkk33@protonmail.com>
2018-06-28 02:26:48 +0200

    Digging in PyQt

--- PyQt/main.py
+++ PyQt/main.py
@@ -5,7 +5,12 @@ from PyQt5 import QtWidgets
 def window():
     app = QtWidgets.QApplication(sys.argv)
     w = QtWidgets.QWidget()
-    w.setWindowTitle('PYQt5 lesson 1')
+    l1 = QtWidgets.QLabel(w)
+    l1.setText("Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit")
+    l1.setAlignment()
+    w.setFixedHeight(540)
+    w.setFixedWidth(960)
+    w.setWindowTitle('PyQt5')
     w.show()
     sys.exit(app.exec_())
 
