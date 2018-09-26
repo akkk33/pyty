612fb1a3734c9c9062c3418ac2dd75653697fbef
akkk33 <akkk33@protonmail.com>
2018-09-27 01:41:55 +0200

    Get the middle character

--- codewars.com/Get the Middle Character.py
+++ codewars.com/Get the Middle Character.py
@@ -1,8 +1,20 @@
+# My humble try:
+# def get_middle(s):
+#     if len(s) % 2 == 0:
+#         print(s[int(len(s) / 2) - 1:int(len(s) / 2) + 1])
+#     else:
+#         print(s[int(len(s) / 2)])
+
+
+# get_middle("Tokyo")
+# get_middle("borrow")
+# get_middle("my")
+# get_middle("A")
+
+
+# Most clever answer found:
 def get_middle(s):
-    if len(s) % 2 == 0:
-        return print(s[int(len(s) / 2) - 1:int(len(s) / 2) + 1])
-    else:
-        return print(s[int(len(s) / 2)])
+    print(s[(len(s) - 1) // 2:len(s) // 2 + 1])
 
 
 get_middle("Tokyo")
