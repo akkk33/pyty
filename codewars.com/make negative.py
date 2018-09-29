9f9f7e1b6ad4fd0b50c9cd7a861b6c9c59cb292a
akkk33 <akkk33@protonmail.com>
2018-09-30 00:27:03 +0200

    Negative number

---
+++ codewars.com/make negative.py
@@ -0,0 +1,13 @@
+# def make_negative(number):
+#     if number > 0:
+#         return - number
+#     else:
+#         return number
+
+
+# Best answer:
+def make_negative(number):
+    return - abs(number)
+
+
+print(make_negative(-2))
