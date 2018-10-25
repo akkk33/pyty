e1730f4bd513ab6ae22327d377b103b8edb589c5
akkk33 <akkk33@protonmail.com>
2018-10-26 01:13:10 +0200

    find the outlier

--- codewars.com/Find The Parity Outlier.py
+++ codewars.com/Find The Parity Outlier.py
@@ -2,24 +2,28 @@
 # array is either entirely comprised of odd integers or entirely comprised of even integers except for a single
 # integer N. Write a method that takes the array as an argument and returns this "outlier" N.
 
+# My solution:
+# def find_stranger(x):
+#     odd, even = [], []
+#     for number in x:
+#         if number % 2 == 1:
+#             odd.append(number)
+#         else:
+#             even.append(number)
+#     if len(odd) == 1:
+#         print(odd)
+#     elif len(even) == 1:
+#         print(even)
 
-def find_stranger(x):
-    odd, even = [], []
-    for number in x:
-        if number % 2 == 1:
-            odd.append(number)
-        else:
-            even.append(number)
-    if len(odd) == 1:
-        print(odd)
-    elif len(even) == 1:
-        print(even)
+# Best solution
+def find_outlier(int):
+    odds = [x for x in int if x%2!=0]
+    evens= [x for x in int if x%2==0]
+    return odds[0] if len(odds)<len(evens) else evens[0]
 
 
 
 
-find_stranger([1, 3, 5, 7, 9, 10, 11, 13, 15])
+print(find_outlier([1, 3, 5, 7, 9, 10, 11, 13, 15]))
 print('-----------------')
-find_stranger([2, 4, 3, 122, 66])
-
-# Something went wrong with codewars!!
+print(find_outlier([2, 4, 3, 122, 66]))
