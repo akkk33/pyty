670f2f4536a1b7f7329c51267d3f7c2d9e481f03
akkk33 <akkk33@protonmail.com>
2018-11-02 07:59:25 +0200

    Added some documentation

--- Data_structure/Linked_lists.py
+++ Data_structure/Linked_lists.py
@@ -1,4 +1,3 @@
-
 class ListNode:
     def __init__(self, val=None):
         self.val = val
@@ -11,32 +10,35 @@ class LinkedList:
 
     def append(self, *args):
         for val in args:
-            node = ListNode(val)
-            current = self.head
+            node = ListNode(val)  # Initialise node
+            current = self.head  # First node by default is head
             while current.next is not None:  # Point to the last node
                 current = current.next
             current.next = node
 
+    # Get the length of linked list
     def length(self):
-        length = 0
-        current = self.head
+        length = 0  # First node index by default is 0 (refers to head)
+        current = self.head  # First node by default is head
         while current.next is not None:
             length += 1
             current = current.next
         return length
 
+    # Get a specific node data by its index
     def get(self, index):
-        if 0 <= index < self.length():
-            target = 0
+        if 0 <= index < self.length():  # Checking if the given index is within the list length
+            target = 0  # Head index
             current = self.head
-            while True:
+            while True:  # Cycle through the list to reach the target node
                 current = current.next
                 if target == index:
-                    return current.val
+                    return current.val  # This is the requested node
                 target += 1
         else:
             return "Index out of range!"
 
+    # Display the list nodes as a regular Python list
     def display(self):
         elements = []
         current = self.head
@@ -47,7 +49,7 @@ class LinkedList:
 
 
 newlist = LinkedList()
-newlist.append(3,5,23,1)
+newlist.append(3, 5, 23, 1)
 newlist.display()
 newlist.length()
 print(newlist.get(2))
